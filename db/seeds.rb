41a28fc41bff6589ce365d4697c62c33f06c44b8
s.golovko <s.golovko@fun-box.ru>
2020-12-31 13:15:35 +0500

    Добавление сидов.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+dataset = [
+  { customer: 'Уфа, улица  Абаканская 16', address: 'Федотова Д. М.' },
+  { customer: 'Уфа, улица  Ажурная 3', address: 'Ежова Э. К.' },
+  { customer: 'Уфа, улица  Академика Филатова 23', address: 'Лаврентьева М. М.' },
+  { customer: 'Уфа, улица  Генеральская 35', address: 'Макеева В. Р.' },
+  { customer: 'Уфа, улица  Георгия Мушникова 32', address: 'Рыжов А. А.' },
+]
+
+dataset.each { |set| Order.find_or_create_by(set) }
